6a2050e9658065ea9abdb07f7bd0cce259f58613
Braien Machado de Paula <braienmp@outlook.com>
2023-11-02 17:09:09 -0300

    refactor: componente PeopleWhoBought

--- islands/Nudge/PeopleWhoBought.tsx
+++ islands/Nudge/PeopleWhoBought.tsx
@@ -1,34 +1,35 @@
-import Nudge, { Position } from "$store/islands/Nudge/Nudge.tsx";
+import Nudge, { NudgeBaseProps } from "$store/islands/Nudge/Nudge.tsx";
 
 export interface Props {
   minQuantityToShow: number;
   quantityOfBought: number;
-  delayToShowInSeconds: number;
-  accentColor?: 'emerald' | 'amber';
-  position?: Position;
-  badgeText: string;
+  nudge?: Partial<NudgeBaseProps>;
 }
 
 function PeopleWhoBought({
-  position = 'right-bottom',
   minQuantityToShow = 100,
   quantityOfBought = 100,
-  badgeText = 'Popular',
-  accentColor = 'emerald',
-  ...props
+  nudge
 }: Props){
+  if (quantityOfBought < minQuantityToShow) return null;
+
+  const nudgeProps: NudgeBaseProps = {
+    position: nudge?.position || 'right-bottom',
+    delayToShowInSeconds: nudge?.delayToShowInSeconds || 0,
+    badge: {
+      text: nudge?.badge?.text || 'Popular',
+      accentColor: nudge?.badge?.accentColor || 'emerald',
+      icon: nudge?.badge?.icon
+    }
+  };
 
   return (
-    <>
-      {quantityOfBought >= minQuantityToShow && (
-        <Nudge {...props} accentColor={accentColor} position={position} badgeText={badgeText}>
-          <p className="text-base font-medium text-zinc-800 tracking-wider leading-relaxed">
-            <strong className="text-emerald-800 font-bold">+{quantityOfBought}</strong> pessoas compraram esse produto recentemente
-          </p>
-        </Nudge>
-      )}
-    </>
-  );
+    <Nudge {...nudgeProps}>
+      <p className="text-base font-medium text-zinc-800 tracking-wider leading-relaxed">
+        <strong className="text-emerald-800 font-bold">+{quantityOfBought}</strong> pessoas compraram esse produto recentemente
+      </p>
+    </Nudge>
+  )
 }
 
 export default PeopleWhoBought
