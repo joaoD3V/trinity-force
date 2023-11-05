22ca61a97d3e3405da713cee689529a4f78175a2
Braien Machado de Paula <braienmp@outlook.com>
2023-11-05 11:49:46 -0300

    feat: usa loader para consultar total de vendas

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -21,13 +21,12 @@ import * as $$14 from "./islands/Nudge/FlashOffer.tsx";
 import * as $$15 from "./islands/Nudge/FreeDelivery.tsx";
 import * as $$16 from "./islands/Nudge/Nudge.tsx";
 import * as $$17 from "./islands/Nudge/NudgeImage.tsx";
-import * as $$18 from "./islands/Nudge/PeopleWhoBought.tsx";
-import * as $$19 from "./islands/OutOfStock.tsx";
-import * as $$20 from "./islands/ProductImageZoom.tsx";
-import * as $$21 from "./islands/SearchControls.tsx";
-import * as $$22 from "./islands/ShippingSimulation.tsx";
-import * as $$23 from "./islands/SliderJS.tsx";
-import * as $$24 from "./islands/WishlistButton.tsx";
+import * as $$18 from "./islands/OutOfStock.tsx";
+import * as $$19 from "./islands/ProductImageZoom.tsx";
+import * as $$20 from "./islands/SearchControls.tsx";
+import * as $$21 from "./islands/ShippingSimulation.tsx";
+import * as $$22 from "./islands/SliderJS.tsx";
+import * as $$23 from "./islands/WishlistButton.tsx";
 
 const manifest = {
   routes: {
@@ -52,13 +51,12 @@ const manifest = {
     "./islands/Nudge/FreeDelivery.tsx": $$15,
     "./islands/Nudge/Nudge.tsx": $$16,
     "./islands/Nudge/NudgeImage.tsx": $$17,
-    "./islands/Nudge/PeopleWhoBought.tsx": $$18,
-    "./islands/OutOfStock.tsx": $$19,
-    "./islands/ProductImageZoom.tsx": $$20,
-    "./islands/SearchControls.tsx": $$21,
-    "./islands/ShippingSimulation.tsx": $$22,
-    "./islands/SliderJS.tsx": $$23,
-    "./islands/WishlistButton.tsx": $$24,
+    "./islands/OutOfStock.tsx": $$18,
+    "./islands/ProductImageZoom.tsx": $$19,
+    "./islands/SearchControls.tsx": $$20,
+    "./islands/ShippingSimulation.tsx": $$21,
+    "./islands/SliderJS.tsx": $$22,
+    "./islands/WishlistButton.tsx": $$23,
   },
   baseUrl: import.meta.url,
 };
