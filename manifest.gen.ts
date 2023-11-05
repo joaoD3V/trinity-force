46027253a69bf43871b29f59a312985576c3a222
Braien Machado de Paula <braienmp@outlook.com>
2023-11-05 10:39:59 -0300

    fix: lógica de loader do componente low stock
    - remove island LowStock
    - move lógica de section LowStock para /sections/nudge
    - remove loader e sdk sem uso

--- manifest.gen.ts
+++ manifest.gen.ts
@@ -2,98 +2,100 @@
 // This file SHOULD be checked into source version control.
 // This file is automatically updated during development when running `dev.ts`.
 
-import * as $$$0 from "./loaders/List/Sections.tsx";
-import * as $$$1 from "./loaders/Layouts/ProductCard.tsx";
-import * as $$$$$$0 from "./sections/Nudge/FreeDelivery.tsx";
-import * as $$$$$$1 from "./sections/Nudge/LowStock.tsx";
-import * as $$$$$$2 from "./sections/Nudge/FlashOffer.tsx";
-import * as $$$$$$3 from "./sections/Nudge/PeopleWhoBought.tsx";
-import * as $$$$$$4 from "./sections/Footer/Footer.tsx";
-import * as $$$$$$5 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$6 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$7 from "./sections/Category/CategoryList.tsx";
-import * as $$$$$$8 from "./sections/Category/CategoryBanner.tsx";
+import * as $$$0 from "./loaders/SingleRequestLoader.ts";
+import * as $$$1 from "./loaders/List/Sections.tsx";
+import * as $$$2 from "./loaders/Layouts/ProductCard.tsx";
+import * as $$$$$$0 from "./sections/Footer/Footer.tsx";
+import * as $$$$$$1 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$2 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$3 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$4 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$5 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$6 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$7 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$8 from "./sections/Product/SearchResult.tsx";
 import * as $$$$$$9 from "./sections/Product/ProductShelfTabbed.tsx";
-import * as $$$$$$10 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$11 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$12 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$13 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$14 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$15 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$16 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$17 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$18 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$19 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$20 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$21 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$22 from "./sections/Gallery.tsx";
-import * as $$$$$$23 from "./sections/Content/Benefits.tsx";
-import * as $$$$$$24 from "./sections/Content/Faq.tsx";
-import * as $$$$$$25 from "./sections/Content/Logos.tsx";
-import * as $$$$$$26 from "./sections/Content/Testimonials.tsx";
-import * as $$$$$$27 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$28 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$29 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$30 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$10 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$11 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$12 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$13 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$14 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$15 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$16 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$17 from "./sections/Layout/Container.tsx";
+import * as $$$$$$18 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$19 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$20 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$21 from "./sections/Nudge/FreeDelivery.tsx";
+import * as $$$$$$22 from "./sections/Nudge/PeopleWhoBought.tsx";
+import * as $$$$$$23 from "./sections/Nudge/LowStock.tsx";
+import * as $$$$$$24 from "./sections/Nudge/FlashOffer.tsx";
+import * as $$$$$$25 from "./sections/Gallery.tsx";
+import * as $$$$$$26 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$27 from "./sections/Content/Faq.tsx";
+import * as $$$$$$28 from "./sections/Content/Benefits.tsx";
+import * as $$$$$$29 from "./sections/Content/Logos.tsx";
+import * as $$$$$$30 from "./sections/Content/Testimonials.tsx";
 import * as $$$$$$31 from "./sections/Header/Header.tsx";
-import * as $$$$$$32 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$33 from "./sections/Theme/Theme.tsx";
-import * as $$$$$$34 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$35 from "./sections/Layout/Container.tsx";
-import * as $$$$$$36 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$37 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$$$$$$0 from "./apps/site.ts";
-import * as $$$$$$$$$$$1 from "./apps/decohub.ts";
+import * as $$$$$$32 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$33 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$34 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$35 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$36 from "./sections/Category/CategoryList.tsx";
+import * as $$$$$$37 from "./sections/Category/CategoryBanner.tsx";
+import * as $$$$$$$$$$$0 from "./apps/decohub.ts";
+import * as $$$$$$$$$$$1 from "./apps/site.ts";
 
 const manifest = {
   "loaders": {
-    "deco-sites/storefront/loaders/Layouts/ProductCard.tsx": $$$1,
-    "deco-sites/storefront/loaders/List/Sections.tsx": $$$0,
+    "deco-sites/storefront/loaders/Layouts/ProductCard.tsx": $$$2,
+    "deco-sites/storefront/loaders/List/Sections.tsx": $$$1,
+    "deco-sites/storefront/loaders/SingleRequestLoader.ts": $$$0,
   },
   "sections": {
-    "deco-sites/storefront/sections/Category/CategoryBanner.tsx": $$$$$$8,
-    "deco-sites/storefront/sections/Category/CategoryList.tsx": $$$$$$7,
-    "deco-sites/storefront/sections/Content/Benefits.tsx": $$$$$$23,
-    "deco-sites/storefront/sections/Content/Faq.tsx": $$$$$$24,
-    "deco-sites/storefront/sections/Content/Logos.tsx": $$$$$$25,
-    "deco-sites/storefront/sections/Content/Testimonials.tsx": $$$$$$26,
-    "deco-sites/storefront/sections/Footer/Footer.tsx": $$$$$$4,
-    "deco-sites/storefront/sections/Gallery.tsx": $$$$$$22,
+    "deco-sites/storefront/sections/Category/CategoryBanner.tsx": $$$$$$37,
+    "deco-sites/storefront/sections/Category/CategoryList.tsx": $$$$$$36,
+    "deco-sites/storefront/sections/Content/Benefits.tsx": $$$$$$28,
+    "deco-sites/storefront/sections/Content/Faq.tsx": $$$$$$27,
+    "deco-sites/storefront/sections/Content/Logos.tsx": $$$$$$29,
+    "deco-sites/storefront/sections/Content/Testimonials.tsx": $$$$$$30,
+    "deco-sites/storefront/sections/Footer/Footer.tsx": $$$$$$0,
+    "deco-sites/storefront/sections/Gallery.tsx": $$$$$$25,
     "deco-sites/storefront/sections/Header/Header.tsx": $$$$$$31,
-    "deco-sites/storefront/sections/Images/BannerGrid.tsx": $$$$$$27,
-    "deco-sites/storefront/sections/Images/Carousel.tsx": $$$$$$28,
-    "deco-sites/storefront/sections/Images/ImageGallery.tsx": $$$$$$29,
-    "deco-sites/storefront/sections/Images/ShoppableBanner.tsx": $$$$$$30,
-    "deco-sites/storefront/sections/Layout/Container.tsx": $$$$$$35,
-    "deco-sites/storefront/sections/Layout/Flex.tsx": $$$$$$36,
-    "deco-sites/storefront/sections/Layout/Grid.tsx": $$$$$$37,
-    "deco-sites/storefront/sections/Layout/GridItem.tsx": $$$$$$34,
-    "deco-sites/storefront/sections/Links/LinkTree.tsx": $$$$$$19,
-    "deco-sites/storefront/sections/Links/Shortcuts.tsx": $$$$$$18,
-    "deco-sites/storefront/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$5,
-    "deco-sites/storefront/sections/Miscellaneous/CookieConsent.tsx": $$$$$$6,
-    "deco-sites/storefront/sections/Newsletter/Newsletter.tsx": $$$$$$32,
-    "deco-sites/storefront/sections/Nudge/FlashOffer.tsx": $$$$$$2,
-    "deco-sites/storefront/sections/Nudge/FreeDelivery.tsx": $$$$$$0,
-    "deco-sites/storefront/sections/Nudge/LowStock.tsx": $$$$$$1,
-    "deco-sites/storefront/sections/Nudge/PeopleWhoBought.tsx": $$$$$$3,
+    "deco-sites/storefront/sections/Images/BannerGrid.tsx": $$$$$$35,
+    "deco-sites/storefront/sections/Images/Carousel.tsx": $$$$$$33,
+    "deco-sites/storefront/sections/Images/ImageGallery.tsx": $$$$$$34,
+    "deco-sites/storefront/sections/Images/ShoppableBanner.tsx": $$$$$$32,
+    "deco-sites/storefront/sections/Layout/Container.tsx": $$$$$$17,
+    "deco-sites/storefront/sections/Layout/Flex.tsx": $$$$$$19,
+    "deco-sites/storefront/sections/Layout/Grid.tsx": $$$$$$20,
+    "deco-sites/storefront/sections/Layout/GridItem.tsx": $$$$$$18,
+    "deco-sites/storefront/sections/Links/LinkTree.tsx": $$$$$$12,
+    "deco-sites/storefront/sections/Links/Shortcuts.tsx": $$$$$$13,
+    "deco-sites/storefront/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$2,
+    "deco-sites/storefront/sections/Miscellaneous/CookieConsent.tsx": $$$$$$1,
+    "deco-sites/storefront/sections/Newsletter/Newsletter.tsx": $$$$$$14,
+    "deco-sites/storefront/sections/Nudge/FlashOffer.tsx": $$$$$$24,
+    "deco-sites/storefront/sections/Nudge/FreeDelivery.tsx": $$$$$$21,
+    "deco-sites/storefront/sections/Nudge/LowStock.tsx": $$$$$$23,
+    "deco-sites/storefront/sections/Nudge/PeopleWhoBought.tsx": $$$$$$22,
     "deco-sites/storefront/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$12,
-    "deco-sites/storefront/sections/Product/ImageGallerySlider.tsx": $$$$$$14,
-    "deco-sites/storefront/sections/Product/NotFound.tsx": $$$$$$15,
-    "deco-sites/storefront/sections/Product/NotFoundChallenge.tsx": $$$$$$13,
-    "deco-sites/storefront/sections/Product/ProductInfo.tsx": $$$$$$17,
-    "deco-sites/storefront/sections/Product/ProductShelf.tsx": $$$$$$10,
+      $$$$$$11,
+    "deco-sites/storefront/sections/Product/ImageGallerySlider.tsx": $$$$$$7,
+    "deco-sites/storefront/sections/Product/NotFound.tsx": $$$$$$5,
+    "deco-sites/storefront/sections/Product/NotFoundChallenge.tsx": $$$$$$4,
+    "deco-sites/storefront/sections/Product/ProductInfo.tsx": $$$$$$6,
+    "deco-sites/storefront/sections/Product/ProductShelf.tsx": $$$$$$3,
     "deco-sites/storefront/sections/Product/ProductShelfTabbed.tsx": $$$$$$9,
-    "deco-sites/storefront/sections/Product/SearchResult.tsx": $$$$$$11,
-    "deco-sites/storefront/sections/Product/Wishlist.tsx": $$$$$$16,
-    "deco-sites/storefront/sections/Social/InstagramPosts.tsx": $$$$$$20,
-    "deco-sites/storefront/sections/Social/WhatsApp.tsx": $$$$$$21,
-    "deco-sites/storefront/sections/Theme/Theme.tsx": $$$$$$33,
+    "deco-sites/storefront/sections/Product/SearchResult.tsx": $$$$$$8,
+    "deco-sites/storefront/sections/Product/Wishlist.tsx": $$$$$$10,
+    "deco-sites/storefront/sections/Social/InstagramPosts.tsx": $$$$$$15,
+    "deco-sites/storefront/sections/Social/WhatsApp.tsx": $$$$$$16,
+    "deco-sites/storefront/sections/Theme/Theme.tsx": $$$$$$26,
   },
   "apps": {
-    "deco-sites/storefront/apps/decohub.ts": $$$$$$$$$$$1,
-    "deco-sites/storefront/apps/site.ts": $$$$$$$$$$$0,
+    "deco-sites/storefront/apps/decohub.ts": $$$$$$$$$$$0,
+    "deco-sites/storefront/apps/site.ts": $$$$$$$$$$$1,
   },
   "name": "deco-sites/storefront",
   "baseUrl": import.meta.url,
--- sections/Nudge/LowStock.tsx
+++ sections/Nudge/LowStock.tsx
@@ -1,1 +1,106 @@
-export { default } from "$store/islands/Nudge/LowStock.tsx";
+import Nudge, { NudgeBaseProps } from "$store/islands/Nudge/Nudge.tsx";
+import TextEditor, {
+  TextEditorProps,
+} from "$store/components/ui/TextEditor.tsx";
+import NudgeImage from "$store/islands/Nudge/NudgeImage.tsx";
+import type { SectionProps } from "deco/mod.ts";
+
+export interface LowStockProps {
+  /**
+   * @title Max items for low stock warning
+   */
+  maxQuantityToShow: number;
+
+  textEditor?: Partial<Omit<TextEditorProps, "accentColor" | "keyProperty">>;
+
+  /**
+   * @title Product image URL
+   */
+  imageURL?: string;
+
+  nudge?: NudgeBaseProps;
+}
+
+export interface Props {
+  stockIntegration: {
+    /**
+     * @title integration url
+     */
+    url: string;
+    /**
+     * @title authentication (including type)
+     */
+    authentication?: string;
+    /**
+     * @title path to needed data
+     */
+    dataPath: string;
+  }
+}
+
+export interface Data {
+  data: string | null
+}
+
+export async function loader(
+  {
+    stockIntegration: {
+      url,
+      authentication,
+      dataPath,
+    },
+    ...props
+  }: Props & LowStockProps,
+  _req: Request
+): Promise<LowStockProps & Data> {
+  if (!url) return { data: null };
+
+  const headers = new Headers();
+
+  if (authentication) {
+    headers.append('Authorization', authentication);
+  }
+
+  const result = await fetch(url, { headers }).then((r) => r.json()).catch((_error) => ({}));
+
+  return { ...props, data: result[dataPath] ?? null };
+}
+
+export default function LowStock({
+  data: stock,
+  maxQuantityToShow,
+  textEditor,
+  imageURL,
+  nudge
+}: SectionProps<typeof loader>) {
+  if (stock > maxQuantityToShow) return null;
+
+  const nudgeProps: NudgeBaseProps = {
+    position: nudge?.position || "bottom-center",
+    delayToShowInSeconds: nudge?.delayToShowInSeconds || 0,
+    badge: {
+      text: nudge?.badge?.text || "ÚLTIMAS UNIDADES",
+      accentColor: nudge?.badge?.accentColor || "amber",
+      icon: nudge?.badge?.icon,
+    },
+    disappearAfterSeconds: nudge?.disappearAfterSeconds,
+    isCloseable: nudge?.isCloseable,
+  };
+
+  const textEditorProps: TextEditorProps = {
+    highlightedText: textEditor?.highlightedText ||
+      "Se apresse! Restam apenas $$ unidades!",
+    keyProperty: stock,
+    accentColor: nudge?.badge?.accentColor || "amber",
+  };
+
+  return (
+    <Nudge {...nudgeProps}>
+      <div className="flex gap-3 items-center">
+        <TextEditor {...textEditorProps} />
+
+        {imageURL && <NudgeImage imageURL={imageURL} />}
+      </div>
+    </Nudge>
+  );
+}
